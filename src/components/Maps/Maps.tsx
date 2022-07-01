9a781c13c2d28f82339f8041b34d63c942b5f155
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-07-01 18:25:53 +0200

    Created Maps Component and Css structure

---
+++ src/components/Maps/Maps.tsx
@@ -0,0 +1,68 @@
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import L, { Icon } from "leaflet";
+import locationsData from "../../data/locations";
+import StyledMaps from "./StyledMaps";
+
+const Maps = () => {
+  const geoIcon = new Icon({
+    iconUrl: "/images/geo-icon.png",
+    iconSize: [46, 46],
+  });
+
+  function MyComponent() {
+    const map = useMapEvents({
+      click: (e) => {
+        const { lat, lng } = e.latlng;
+        L.marker([lat, lng], { icon: geoIcon }).addTo(map);
+      },
+    });
+    return null;
+  }
+
+  return (
+    <StyledMaps className="leaflet-container">
+      <MapContainer
+        center={[41.346176, 2.168365]}
+        zoom={13}
+        scrollWheelZoom={true}
+        className="map"
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {locationsData.features.map((location) => (
+          <Marker
+            key={location.properties.LOCATION_ID}
+            position={[
+              location.geometry.coordinates[0],
+              location.geometry.coordinates[1],
+            ]}
+            icon={geoIcon}
+          >
+            <Popup
+              position={[
+                location.geometry.coordinates[0],
+                location.geometry.coordinates[1],
+              ]}
+            >
+              <div>
+                <h2>{location.properties.NAME}</h2>
+                <p>{location.properties.DESCRIPTIO}</p>
+              </div>
+            </Popup>
+            <MyComponent />
+          </Marker>
+        ))}
+      </MapContainer>
+    </StyledMaps>
+  );
+};
+
+export default Maps;
