98a452145828ce8ec94e574ffad67539dc19302f
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-07-11 16:08:18 +0200

    Created user login and register thunks

--- src/types/types.ts
+++ src/types/types.ts
@@ -7,3 +7,24 @@ export interface UserState {
   userInfo: User;
   logged: boolean;
 }
+
+export interface UserRegister {
+  username: string;
+  password: string;
+}
+
+export interface UserLogin {
+  username: string;
+  password: string;
+}
+
+export interface DecodeToken {
+  username: string;
+  id: string;
+}
+
+export interface ResponseApi {
+  data: {
+    token: string;
+  };
+}
